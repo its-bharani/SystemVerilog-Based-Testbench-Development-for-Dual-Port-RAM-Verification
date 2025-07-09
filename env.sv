85e6ec2258a04e494e178452eefefa90f1044316
J Bharani <mail2jbharani@gmail.com>
2025-07-09 15:13:58 +0530

    Update env.sv

--- env.sv
+++ env.sv
@@ -12,28 +12,32 @@ mailbox #(reg_trans)ref2sb=new();
   mailbox #(reg_trans)rdmon2sb=new();
   mailbox #(reg_trans)gen2rddr=new();
 
-reg_gen gen_h;
-reg_wr_mon wrmon_h;
-reg_drv dri_h;
-reg_rd_mon rdmon_h;
-reg_sb sb_h;
+ram_gen gen_h;
+ram_wr_mon wrmon_h;
+ram_wr_drv wrdri_h;
+ram_rd_mon rdmon_h;
+ram_rd_drv rddri_h;
+ram_sb sb_h;
 ref_model model_h;
 
-function new(virtual reg_if.DRV dr_if,
-virtual reg_if.WR_MON wrmon_if,
-virtual reg_if.RD_MON rdmon_if);
-        this.dr_if=dr_if;
-        this.wrmon_if=wrmon_if;
-        this.rdmon_if=rdmon_if;
+  function new(virtual reg_if.WR_DRV wr_dr_if,
+virtual reg_if.RD_DRV rd_dr_if,
+virtual reg_if.WR_MON wr_mon_if,
+virtual reg_if.RD_MON rd_mon_if);
+        this.wr_dr_if=wr_dr_if;
+        this.rd_dr_if=rd_dr_if;
+        this.wr_mon_if=wr_mon_if;
+        this.rd_mon_if=rd_mon_if;
 endfunction
 
 virtual task build();
-        gen_h=new(gen2dr);
-        dri_h=new(dr_if,gen2dr,drv2mon);
-        wrmon_h=new(wrmon_if,mon2rm,drv2mon);
-        rdmon_h=new(rdmon_if,mon2sb);
+  gen_h=new(gen2wrdr,gen2rddr);
+  wrdri_h=new(wr_dr_if,gen2wrdr);
+  wrmon_h=new(wr_mon_if,wrmon2rm);
+  rddri_h=new(rd_dr_if,gen2rddr);
+  rdmon_h=new(rd_mon_if,rdmon2sb);
         model_h=new(mon2rm,ref2sb);
-        sb_h=new(ref2sb,mon2sb);
+  sb_h=new(ref2sb,rdmon2sb);
 endtask
 virtual task clear_duv();
         @(dr_if.dr_cb);
