7d9698add869818c4b3dea285b0a63cde855592f
J Bharani <mail2jbharani@gmail.com>
2025-07-09 12:18:27 +0530

    Update wr_monitor.sv

--- wr_monitor.sv
+++ wr_monitor.sv
@@ -1,16 +1,16 @@
 class ram_wr_mon;
 
-virtual reg_if.WR_MON wrmon_if;
+virtual reg_if.WR_MON wr_mon_if;
 
 reg_trans data2rm=new();
 reg_trans wr_data=new();
 
         mailbox #(reg_trans)wrmon2rm;
 //mailbox #(reg_trans)drv2mon;
-function new(virtual reg_if.WR_MON wrmon_if,
+        function new(virtual reg_if.WR_MON wr_mon_if,
              mailbox #(reg_trans)wrmon2rm);
 begin
-        this.wrmon_if=wrmon_if;
+        this.wr_mon_if=wr_mon_if;
         //this.drv2mon=drv2mon;
         this.wrmon2rm=wrmon2rm;
 //this.wr_data=new;
@@ -19,11 +19,11 @@ endfunction
 
 virtual task monitor();
 begin
-        @(wrmon_if.wr_mon_cb);
+        @(wr_mon_if.wr_mon_cb);
                 begin
-                        wr_data.in<=wrmon_if.wr_mon_cb.in;
-                        wr_data.wr<=wrmon_if.wr_mon_cb.wr;
-                        wr_data.wr_add<=wrmon_if.wr_mon_cb.wr_add;
+                        wr_data.in<=wr_mon_if.wr_mon_cb.in;
+                        wr_data.wr<=wr_mon_if.wr_mon_cb.wr;
+                        wr_data.wr_add<=wr_mon_if.wr_mon_cb.wr_add;
                 end
 end
 endtask
