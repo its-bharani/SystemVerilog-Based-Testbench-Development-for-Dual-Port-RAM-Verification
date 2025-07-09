dfb030e090533578ab085852c5290c39473ae5b9
J Bharani <mail2jbharani@gmail.com>
2025-07-09 15:03:36 +0530

    Update generator.sv

--- generator.sv
+++ generator.sv
@@ -4,7 +4,9 @@ reg_trans trans_h;
 reg_trans data2send;
 int no_of_trans=100;
   mailbox #(reg_trans)gen2wrdr;
-  function new(mailbox #(reg_trans)gen2wrdr);
+  mailbox #(reg_trans)gen2rddr;
+  function new(mailbox #(reg_trans)gen2wrdr,
+               mailbox #(reg_trans)gen2rddr);
         this.gen2wrdr=gen2wrdr;
 endfunction
 
@@ -15,7 +17,8 @@ repeat(no_of_trans)begin
         trans_h=new;
         assert(trans_h.randomize());
         data2send=new trans_h;//shallow copy
-        gen2wrdr.put(data2send);//put data into mailbox
+        gen2wrdr.put(data2send);
+        gen2rddr.put(data2send);//put data into mailbox
 end
 endtask
 endclass
