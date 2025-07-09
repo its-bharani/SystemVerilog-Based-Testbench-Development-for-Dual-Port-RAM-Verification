4661a8e30a70474ebdb3d0a40a6d06c192cd4918
J Bharani <mail2jbharani@gmail.com>
2025-07-09 11:59:57 +0530

    Create wr_driver.sv

---
+++ wr_driver.sv
@@ -0,0 +1,36 @@
+class wr_drv;
+
+virtual reg_if.WR_DRV wr_dr_if;//virtual interface
+
+reg_trans data2duv=new();
+
+  mailbox #(reg_trans)gen2wrdr;
+
+  function new(virtual reg_if.WR_DRV wr_dr_if,mailbox #(reg_trans)gen2wrdr);
+
+        this.wr_dr_if=wr_dr_if;
+        this.gen2wrdr=gen2wrdr;
+endfunction
+
+virtual task drive();
+  begin@(wr_dr_if.wr_dr_cb)begin
+                wr_dr_if.wr_dr_cb.in<=data2duv.in;
+                wr_dr_if.wr_dr_cb.wr<=data2duv.wr;
+               wr_dr_if.wr_dr_cb.wr_add<=data2duv.wr_add;
+                
+end
+end
+endtask
+
+virtual task start();
+fork forever
+        begin
+                gen2wrdr.get(data2duv);//get data from mailbox
+                drive();
+//              drv2mon.put(data2duv);//put into mailbox
+//              data2duv.display("at driver----generated data----------------");
+end
+join_none
+
+endtask
+endclass
