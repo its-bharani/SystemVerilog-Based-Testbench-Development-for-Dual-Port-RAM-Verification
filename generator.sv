8317efb48597d8180e7d563cfb7706509767556b
J Bharani <mail2jbharani@gmail.com>
2025-07-09 11:52:31 +0530

    Create generator.sv

---
+++ generator.sv
@@ -0,0 +1,22 @@
+class reg_gen;
+
+reg_trans trans_h;
+reg_trans data2send;
+int no_of_trans=100;
+  mailbox #(reg_trans)gen2wrdr;
+  function new(mailbox #(reg_trans)gen2wrdr);
+        this.gen2wrdr=gen2wrdr;
+endfunction
+
+
+virtual task start();
+repeat(no_of_trans)begin
+
+        trans_h=new;
+        assert(trans_h.randomize());
+        data2send=new trans_h;//shallow copy
+        gen2wrdr.put(data2send);//put data into mailbox
+end
+endtask
+endclass
+~
