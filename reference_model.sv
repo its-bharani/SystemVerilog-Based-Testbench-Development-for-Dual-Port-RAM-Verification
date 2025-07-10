1f3d84d918f2e5acd7201361af2c0eaa691c9b54
J Bharani <mail2jbharani@gmail.com>
2025-07-10 21:10:10 +0530

    Update reference_model.sv

--- reference_model.sv
+++ reference_model.sv
@@ -1,46 +1,48 @@
 class ref_model;
-
 ram_trans w_data=new();
-  static bit [63:0] mem [0:4095];
+ram_trans r_data=new();
+ram_trans m_data=new;
+  static logic [63:0] mem [0:4095];
 
-  mailbox #(reg_trans)wrmon2rm;
-  mailbox #(reg_trans)rdmon2ref;
-  mailbox #(reg_trans)ref2sb;
+  mailbox #(ram_trans)wrmon2rm;
+  mailbox #(ram_trans)rdmon2ref;
+  mailbox #(ram_trans)ref2sb;
 
-  function new(mailbox #(reg_trans)wrmon2rm,mailbox #(reg_trans)rdmon2ref,mailbox #(reg_trans)ref2sb);
-        this.mon2rm=mon2rm;
+  function new(mailbox #(ram_trans)wrmon2rm,mailbox #(ram_trans)rdmon2ref,mailbox #(ram_trans)ref2sb);
+        this.wrmon2rm=wrmon2rm;
         this.rdmon2ref=rdmon2ref;
         this.ref2sb=ref2sb;
 //      this.w_data=new();
 endfunction
 
-virtual task ram_mod(reg_trans w_data);
-        begin
-          if (w_data.wr) begin
-            mem[w_data.wr_add] <= w_data.in;
+virtual task ram_mod(ram_trans m_data);
+if (m_data.wr) begin
+            mem[m_data.wr_add]= m_data.in;
         end
-    end
-  if (w_data.rd) begin
-    w_data.out <= mem[w_data.rd_add];
+  if (m_data.rd) begin
+    m_data.out = mem[m_data.rd_add];
         end else begin
-            w_data.out <= 64'd0;
+            m_data.out = 64'd0;
 
-end
+    end
 endtask
 virtual task start();
-fork
-begin
-        forever
+fork        forever
                 begin
-                        wrmon2rm.get(w_data);
-                  rdmon2ref.get(w_data);
-                ram_mod(w_data);//w_data.display("from generator the data generated------");
-                  $display("ref_out=%b",w_data.out);//just to check
+                wrmon2rm.get(w_data);
+                rdmon2ref.get(r_data);
+                m_data.wr<=w_data.wr;
+                m_data.wr_add<=w_data.wr_add;
+                m_data.in<=w_data.in;
+                m_data.rd<=r_data.rd;
+                m_data.rd_add<=r_data.rd_add;
+                ram_mod(m_data);//m_data.display("from reference model the data generated------");
+             //     $display("ref_out=%d",m_data.out);//just to check
                         //w_data.out=ref_out;
-                        ref2sb.put(w_data);
+                        ref2sb.put(m_data);
 //                      w_data.display("from reference_model");
-end
 end
 join_none
 endtask
 endclass
+
