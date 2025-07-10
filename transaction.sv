7fedd887e39f4b94588b8f3205971c2694c9bce7
J Bharani <mail2jbharani@gmail.com>
2025-07-10 21:11:32 +0530

    Update transaction.sv

--- transaction.sv
+++ transaction.sv
@@ -1,18 +1,18 @@
 class ram_trans;
 
-rand logic wr,rd;
-rand logic [11:0]wr_add,rd_add;
+rand logic wr;
+rand logic rd;
+rand logic [11:0]wr_add;
+rand logic [11:0]rd_add;
 rand logic [63:0]in;
   logic [63:0]out;//output
   //constraint
-  constraint in_range_c {in inside {[64'h0000_0000_0000_0000 : 64'hFFFF_FFFF_FFFF_FFFF]};
-  }
+  constraint in_range_c {in inside {[64'h0000_0000_0000_0000 : 64'hFFFF_FFFF_FFFF_FFFF]};}
 
-  constraint addr_valid_c {wr_add inside {[0 : 4095]};rd_add inside {[0 : 4095]};}
-  constraint wr_rd_distribution { wr dist{0:=70, 1:=30}; rd dist{0:=30, 1:=70};}
-
-endclass
-
+  constraint addr_valid_c {wr_add inside {[0 : 20]};}//checking operation in first 20 locations
+  constraint addr_valid_d {rd_add inside {[0 : 20]};}
+  constraint wr_distribution {wr dist{0:=70, 1:=30};}
+  constraint rd_distribution {rd dist{0:=30, 1:=70};}
 
 virtual function void display(input string s);
 begin
@@ -27,3 +27,4 @@ end
 endfunction
 
 endclass:ram_trans
+
